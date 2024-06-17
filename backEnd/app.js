9786208c59dc0af1222f9399c82a5753524917e8
guiback0 <guillaume.zimmer.pro@gmail.com>
2024-06-17 13:53:04 +0200

    mongodb atlas connect

--- backEnd/app.js
+++ backEnd/app.js
@@ -1,9 +1,39 @@
-const express = require('express');
+const express = require("express");
 
+const mongoose = require('mongoose');
+const uri = "mongodb+srv://guiback:Dihupu2102@sandbox.kkyhzdb.mongodb.net/?retryWrites=true&w=majority&appName=SandBox";
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+async function run() {
+  try {
+    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+    await mongoose.connect(uri, clientOptions);
+    await mongoose.connection.db.admin().command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await mongoose.disconnect();
+  }
+}
+run().catch(console.dir);
 const app = express();
 
-app.use((req, res) => {
-   res.json({ message: 'Votre requête a bien été reçue !' }); 
-});
+app.use((req, res, next) => {
+    console.log('Requête reçue !');
+    next();
+  });
+  
+  app.use((req, res, next) => {
+    res.status(201);
+    next();
+  });
+  
+  app.use((req, res, next) => {
+    res.json({ message: 'Votre requête a bien été reçue !' });
+    next();
+  });
+  
+  app.use((req, res, next) => {
+    console.log('Réponse envoyée avec succès !');
+  });
 
 module.exports = app;
