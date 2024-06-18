bb6eda1613a3d3209619f86e4e26ba98b57cac75
guiback0 <guillaume.zimmer.pro@gmail.com>
2024-06-18 08:56:32 +0200

    middleware for cors error

--- backEnd/app.js
+++ backEnd/app.js
@@ -1,39 +1,58 @@
 const express = require("express");
 
-const mongoose = require('mongoose');
-const uri = "mongodb+srv://guiback:Dihupu2102@sandbox.kkyhzdb.mongodb.net/?retryWrites=true&w=majority&appName=SandBox";
-const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
-async function run() {
-  try {
-    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
-    await mongoose.connect(uri, clientOptions);
-    await mongoose.connection.db.admin().command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await mongoose.disconnect();
-  }
-}
-run().catch(console.dir);
 const app = express();
 
+const mongoose = require("mongoose");
+const uri =
+   "mongodb+srv://guiback:Dihupu2102@sandbox.kkyhzdb.mongodb.net/?retryWrites=true&w=majority&appName=SandBox";
+const clientOptions = {
+   serverApi: { version: "1", strict: true, deprecationErrors: true },
+};
+async function run() {
+   try {
+      // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+      await mongoose.connect(uri, clientOptions);
+      await mongoose.connection.db.admin().command({ ping: 1 });
+      console.log(
+         "Pinged your deployment. You successfully connected to MongoDB!"
+      );
+   } finally {
+      // Ensures that the client will close when you finish/error
+      await mongoose.disconnect();
+   }
+}
+run().catch(console.dir);
+
 app.use((req, res, next) => {
-    console.log('Requête reçue !');
-    next();
-  });
-  
-  app.use((req, res, next) => {
-    res.status(201);
-    next();
-  });
-  
-  app.use((req, res, next) => {
-    res.json({ message: 'Votre requête a bien été reçue !' });
-    next();
-  });
-  
-  app.use((req, res, next) => {
-    console.log('Réponse envoyée avec succès !');
-  });
+   res.setHeader("Access-Control-Allow-Origin", "*");
+   res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+   );
+   res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+   );
+   next();
+});
+
+app.use((req, res, next) => {
+   console.log("Requête reçue !");
+   next();
+});
+
+app.use((req, res, next) => {
+   res.status(201);
+   next();
+});
+
+app.use((req, res, next) => {
+   res.json({ message: "Votre requête a bien été reçue !" });
+   next();
+});
+
+app.use((req, res, next) => {
+   console.log("Réponse envoyée avec succès !");
+});
 
 module.exports = app;
